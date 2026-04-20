6f4532ab2e0320a6055715aeab910bf410753656
ballinpeanut <miltonmark.mh@gmail.com>
2026-04-19 21:52:39 -0500

    draw chess pieces onto the chessboard that was previously created.

--- gui.py
+++ gui.py
@@ -18,11 +18,47 @@ def draw_board(screen):
 
             pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+def load_images():
+    colors = ['white', 'black']
+    pieces = ['king', 'queen', 'rook', 'bishop', 'knight', 'pawn']
+    images = {}
+    
+    for color in colors:
+        for piece in pieces:
+          image = pygame.image.load(f"assets/chessPieces/{color}-{piece}.png")
+          image = pygame.transform.scale(image, (SQUARE_SIZE, SQUARE_SIZE)) 
+          images[f"{color}-{piece}"] = image 
+    
+    return images
+
+def draw_pieces(screen, images, board):
+    for square, piece in board.items():
+        if piece is not None:
+            piece_type = piece.get_type()
+            piece_color = piece.get_color().lower()
+            
+            image = images[f"{piece_color}-{piece_type}"]
+            
+            col_ind = ord(square[0]) - ord('a')
+            row_ind = 8 - int(square[1])
+            
+            x = col_ind * SQUARE_SIZE
+            y = row_ind * SQUARE_SIZE
+            
+            screen.blit(image, (x, y))
+    
 
 # run game
+game = ChessVar()
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Chess")
+
+images = load_images()
+
+# test
+# print(game._chessboard['e1'].get_type())
+# print(game._chessboard['e1'].get_color())
 
 while True:
     for event in pygame.event.get():
@@ -30,5 +66,6 @@ while True:
             pygame.quit()
         
     draw_board(screen)
+    draw_pieces(screen, images, game._chessboard)
     pygame.display.flip()
         
